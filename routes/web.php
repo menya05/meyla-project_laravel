ef52b247bbdbe984b0aaf4371c400c08c293a36f
menya05 <meylaarista01@gmail.com>
2024-10-16 11:24:07 +0700

    add index dan login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/login', function () {
+    return view('login');
+});
